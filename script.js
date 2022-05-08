3e98d62c45183914c07326c8a3a0a8bb6d205401
userNameAsString <99268849+userNameAsString@users.noreply.github.com>
2022-05-08 17:26:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function mobileMode() {
+  for (let i = 0; i < document.getElementsByClassName("mode").length ; i++){
+    document.getElementsByClassName("mode")[i].style.margin = "3% 7%";
+    document.getElementsByClassName("mode")[i].style.width = "35%";
+  }
+  document.getElementById("p").innerHTML='<img class = "icon" alt="Mobile mode" src="phone.png">';
+  document.getElementById("c").innerHTML='<img class = "icon" alt="Computer mode" src="computer.png">';
+    for (let i = 0; i < document.getElementsByClassName("normal").length ; i++){
+      document.getElementsByClassName("normal")[i].style.width = "99.7%";
+    }
+    for (let i = 0; i < document.getElementsByClassName("endPageButton").length ; i++){
+        document.getElementsByClassName("endPageButton")[i].style.width = "30%";
+        document.getElementsByClassName("endPageButton")[i].style.margin = "0.3%";
+    }
+    document.getElementById("h").innerHTML='<img class = "icon2" alt="Home" src="home.png">';
+    document.getElementById("b").innerHTML='<img class = "icon2" alt="Back" src="back.png">';
+    document.getElementById("n").innerHTML='<img class = "icon2" alt="Next" src="next.png">';
+
+    for (let i = 0; i < document.getElementsByClassName("empty").length ; i++){
+        document.getElementsByClassName("empty")[i].style.width = "0.1%";
+    }
+
+  }
+function computerMode() {
+    for (let i = 0; i < document.getElementsByClassName("mode").length ; i++){
+      document.getElementsByClassName("mode")[i].style.margin = "4px 2px";
+      document.getElementsByClassName("mode")[i].style.width = "48%";
+    }
+    document.getElementById("p").innerHTML='Mobile mode';
+    document.getElementById("c").innerHTML='Computer mode';
+
+    for (let i = 0; i < document.getElementsByClassName("normal").length ; i++){
+      document.getElementsByClassName("normal")[i].style.width = "32.9%";
+    }
+    for (let i = 0; i < document.getElementsByClassName("endPageButton").length ; i++){
+        document.getElementsByClassName("endPageButton")[i].style.width = "20%";
+    }
+    for (let i = 0; i < document.getElementsByClassName("empty").length ; i++){
+        document.getElementsByClassName("empty")[i].style.width = "20%";
+    }
+    document.getElementById("h").innerHTML='Home';
+    document.getElementById("b").innerHTML='Back';
+    document.getElementById("n").innerHTML='Next';
+  }
+computerMode()
